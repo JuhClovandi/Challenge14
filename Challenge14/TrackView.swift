309866967ce3c1def65193be222ff01f9cecb8ce
Keitiely Silva <keitielysilvaviana@gmail.com>
2026-05-15 17:47:37 -0300

    :construction: responsividade em processo..

--- Challenge14/TrackView.swift
+++ Challenge14/TrackView.swift
@@ -62,12 +62,22 @@ class TrackView: UIView {
         return view
     }()
     
+    //MARK: RowStackView
+    let rowStackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+        view.alignment = .leading
+        view.distribution = .equalSpacing
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     //MARK: INFOSTACKVIEW
     //UIStackView - InfoStackView
     let infoStackView: UIStackView = {
         let view = UIStackView()
-        view.axis = .horizontal
-        view.alignment = .center
+        view.axis = .vertical
+        view.alignment = .leading
         view.distribution = .equalSpacing
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
@@ -124,7 +134,8 @@ class TrackView: UIView {
     //UIStackView - botao de ordem aleatoria
     let shuffleButton: UIButton = {
             let view = UIButton()
-            view.setImage(UIImage(systemName: "shuffle"), for: .normal)
+            let conf = UIImage.SymbolConfiguration(pointSize: 20, weight: .regular)
+        view.setImage(UIImage(systemName: "shuffle", withConfiguration: conf, for: .normal)
             view.tintColor = .white
             view.translatesAutoresizingMaskIntoConstraints = false
             return view
@@ -133,7 +144,7 @@ class TrackView: UIView {
     //UIStackView - botao de voltar musica
     let previousButton: UIButton = {
             let view = UIButton()
-            view.setImage(UIImage(systemName: "backward.fill"), for: .normal)
+            view.setImage(UIImage(systemName: "backward.end.fill"), for: .normal)
             view.tintColor = .white
             view.translatesAutoresizingMaskIntoConstraints = false
             return view
@@ -151,11 +162,12 @@ class TrackView: UIView {
     //UIStackView - avançar musica
     let nextButton: UIButton = {
            let view = UIButton()
-           view.setImage(UIImage(systemName: "forward.fill"), for: .normal)
+           view.setImage(UIImage(systemName: "forward.end.fill"), for: .normal)
            view.tintColor = .white
            view.translatesAutoresizingMaskIntoConstraints = false
            return view
        }()
+    
     //UIStackView - repetir musica
     let repeatButton: UIButton = {
             let view = UIButton()
@@ -166,6 +178,77 @@ class TrackView: UIView {
         }()
     
     //MARK: aqui contem tres buttons -
+    let footerStackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+        view.distribution = .equalSpacing
+        view.alignment = .center
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    // Stack vertical: ícone bluetooth + label
+    let bluetoothContainerStack: UIStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+        view.alignment = .center
+        view.spacing = 4
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+   //button de bluetooth
+    let bluetoothButton: UIButton = {
+        let view = UIButton()
+
+        let image = UIImage(
+            named: "bluetooth"
+            )
+        view.setImage(image, for: .normal)
+        view.imageView?.contentMode = .scaleAspectFit
+        view.contentVerticalAlignment = .fill
+        view.contentHorizontalAlignment = .fill
+        view.translatesAutoresizingMaskIntoConstraints = false
+      
+        return view
+    }()
+    
+    let textBluetooth: UILabel = {
+        let label = UILabel()
+        label.text = "BEATSPILL+"
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // Stack horizontal: share + list
+    let rightButtonsStack: UIStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+        view.alignment = .center
+        view.spacing = 24
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    //button de compartilhar
+    let sharedButton: UIButton = {
+        let view = UIButton()
+        view.setImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
+        view.tintColor = .white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    //butto de list
+    let listButton: UIButton = {
+        let view = UIButton()
+        view.setImage(UIImage(systemName: "list.bullet"), for: .normal)
+        view.tintColor = .white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
     //MARK: LETRA DA MUSICA
     //UIStackView - Container texto da Letra Musica
@@ -201,10 +284,10 @@ class TrackView: UIView {
     }
     //so pra definir cor fundo
     private func setupBackground(){
-        backgroundColor = .red
+        backgroundColor = .gray
     }
     
-    //hierarquia dos modulos
+    //hierarquia dos módulos
     ///views soltas na root → addSubview()
     /// views dentro de UIStackView → addArrangedSubview()
     private func setupHierarchy(){
@@ -217,11 +300,14 @@ class TrackView: UIView {
         //ImageView
         addSubview(capaMusicaView)
         
-        //InfoStackView
-        addSubview(infoStackView)
+        //RowStackView
+        addSubview(rowStackView)
+        rowStackView.addArrangedSubview(infoStackView)
+        rowStackView.addArrangedSubview(likeButton)
+        
         infoStackView.addArrangedSubview(tituloMusicaLabel)
         infoStackView.addArrangedSubview(nomeCantorLabel)
-        infoStackView.addArrangedSubview(likeButton)
+       
         
         //ProgressBar
         addSubview(progressBar)
@@ -234,7 +320,19 @@ class TrackView: UIView {
         buttonStackView.addArrangedSubview(nextButton)
         buttonStackView.addArrangedSubview(repeatButton)
         
-        //falta tres buttons aqui.. adicionar depois
+        //footer de tres buttons
+        addSubview(footerStackView)
+        // bluetooth + texto juntos (vertical)
+        bluetoothContainerStack.addArrangedSubview(bluetoothButton)
+        bluetoothContainerStack.addArrangedSubview(textBluetooth)
+        
+        // botões da direita
+        rightButtonsStack.addArrangedSubview(sharedButton)
+        rightButtonsStack.addArrangedSubview(listButton)
+
+        footerStackView.addArrangedSubview(bluetoothContainerStack)
+        footerStackView.addArrangedSubview(rightButtonsStack)
+        
         
         //Letra da musica
         addSubview(textLetraMusica)
@@ -262,14 +360,14 @@ class TrackView: UIView {
             capaMusicaView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.88),
             capaMusicaView.heightAnchor.constraint(equalTo: capaMusicaView.widthAnchor),
 
-            // INFO (titulo + artista + like)
-            infoStackView.topAnchor.constraint(equalTo: capaMusicaView.bottomAnchor, constant: 24),
-            infoStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
-            infoStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
-            infoStackView.heightAnchor.constraint(equalToConstant: 50),
-
+            // ROWSTACKVIEW (info + Like)
+            rowStackView.topAnchor.constraint(equalTo: capaMusicaView.bottomAnchor, constant: 24),
+            rowStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+            rowStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
+            rowStackView.heightAnchor.constraint(equalToConstant: 50),
+            
             // PROGRESS BAR
-            progressBar.topAnchor.constraint(equalTo: infoStackView.bottomAnchor, constant: 16),
+            progressBar.topAnchor.constraint(equalTo: rowStackView.bottomAnchor, constant: 16),
             progressBar.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
             progressBar.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
 
@@ -278,9 +376,23 @@ class TrackView: UIView {
             buttonStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
             buttonStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
             buttonStackView.heightAnchor.constraint(equalToConstant: 64),
+            
+            playPauseButton.widthAnchor.constraint(equalToConstant: 80),
+            playPauseButton.heightAnchor.constraint(equalToConstant: 80),
 
+           
+            //FooterStackView
+            footerStackView.topAnchor.constraint(equalTo: buttonStackView.bottomAnchor, constant: 24),
+            footerStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+            footerStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
+            footerStackView.heightAnchor.constraint(equalToConstant: 60),
+            
+            bluetoothButton.heightAnchor.constraint(equalToConstant: 30),
+            bluetoothButton.widthAnchor.constraint(equalToConstant: 30),
+            
+            
             // LETRA
-            textLetraMusica.topAnchor.constraint(equalTo: buttonStackView.bottomAnchor, constant: 24),
+            textLetraMusica.topAnchor.constraint(equalTo: footerStackView.bottomAnchor, constant: 16),
             textLetraMusica.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
             textLetraMusica.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
             textLetraMusica.heightAnchor.constraint(equalToConstant: 50),
