807d65b32c360366123601a12b2c65bb97cda4cb
WiseGhostan <wiseghostan@protonmail.com>
2026-05-19 15:17:45 -0300

    Finished player, with support for different screen sizes, font sizes mostly working.

--- Challenge14/Joao/Components/Fonts.swift
+++ Challenge14/Joao/Components/Fonts.swift
@@ -5,3 +5,20 @@
 //  Created by Wise on 18/05/26.
 //
 
+import UIKit
+
+let spotifyFont: [UIFont.TextStyle: UIFont] = [
+    .largeTitle: UIFont(name: "Avenir-Next-Bold", size: 24)!,
+    .title1: UIFont(name: "Avenir-Next-Bold", size: 19)!,
+    .body: UIFont(name: "Avenir-Next-Demi-Bold", size: 12)!,
+    .callout: UIFont(name: "Avenir-Next", size: 10)!
+]
+
+extension UIFont {
+     class func customFont(for textStyle: UIFont.TextStyle) -> UIFont {
+        let spotifyFont = spotifyFont[textStyle]!
+        let metrics = UIFontMetrics(forTextStyle: textStyle)
+        let scaledFont = metrics.scaledFont(for: spotifyFont)
+        return scaledFont
+    }
+}
